719c30951a9d152fb981b79472009db62e7b822a
Valentin Barit <valentin.barit@gmail.com>
2023-06-23 01:05:48 +0200

    fix: 🐛 branding ctrl up

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import ctrlUpLogo from "/ctrlup.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://ctrlup.dev" target="_blank">
+          <img src={ctrlUpLogo} className="logo" alt="Ctrl Up logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Ctrl Up</h1>
+      <p>
+        <i>Des outils pour les nuls</i> suivi{" "}
+        <i>Déploiement les doigts dans le nez</i>
+      </p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,11 +25,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Ctrl Up logo to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
